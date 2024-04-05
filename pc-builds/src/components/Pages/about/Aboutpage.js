b626025f6c1888f8c59288c4a57f27759710d9c4
Cece209 <143728721+Cece209@users.noreply.github.com>
2024-04-05 17:13:16 -0500

    Updated the about page
    I added what our website is about.

--- pc-builds/src/components/Pages/about/Aboutpage.js
+++ pc-builds/src/components/Pages/about/Aboutpage.js
@@ -10,7 +10,13 @@ function AboutPage(){
                 <Col xs={12} md={6}>
                     <h1 style={{ color: 'white' }}>About Us</h1>
                     <p style={{ color: 'white' }}>
-                        Insert your about content here.
+                    "Building Dreams," a PC integration service website that offers a personalized computer assembly experience. The website aims to provide an intuitive PC configurator system, 
+                    enabling users to customize and visualize their ideal computer setup. 
+                    Users will have the flexibility to select various components like processors, graphics cards, memory, storage, and peripherals. 
+                    The system will ensure component compatibility, offer dynamic price calculations, and provide a user-friendly interface for an enhanced customization process. 
+                    Additionally, the website will feature a chat/message functionality for personalized assistance, allowing users to seek advice and interact with our representatives. 
+                    This platform is designed to cater to tech enthusiasts, offering them a space to not only build their dream PCs but also share and save their configurations for future reference. 
+                    The ultimate goal is to combine passion for technology with expert knowledge, delivering a comprehensive and enjoyable PC building experience.
                     </p>
                 </Col>
                 <Col xs={12} md={6} className="text-center">
