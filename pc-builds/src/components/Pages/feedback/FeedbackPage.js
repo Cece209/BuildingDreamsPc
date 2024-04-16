2de4a6f528640c13e42a2f370ca08092df56a09d
epalom2 <epalom2@lsu.edu>
2024-04-16 13:45:10 -0500

    started feedback features

--- pc-builds/src/components/Pages/feedback/FeedbackPage.js
+++ pc-builds/src/components/Pages/feedback/FeedbackPage.js
@@ -1,14 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Container } from "react-bootstrap";
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+
+import { Rating } from '@aws-amplify/ui-react';
+
+
+
+  
 
 function FeedbackPage(){
-    return(
+    const [rating, setRating] = useState(0); // Initializes state to hold rating value
+    
+
+    const handleSubmitFeedback = () => {
+        // Here you might call an API to save the rating
+        console.log('Feedback submitted with rating:', rating);
+        // Optionally reset the rating or show a message
+        setRating(0);
+        alert('Thank you for your feedback!');
+    };
+    
+
+
+    return (
         <Container>
             <Row className="px-4 my-5">
-            <Col><h1 style={{ color: 'white', textShadow: '0 0 3px black' }}>Feedback</h1></Col>
+                <Col><h1 style={{ color: 'white', textShadow: '0 0 3px black' }}>Feedback</h1></Col>
             </Row>
+            <Row className="px-4">
+                <Col>
+                    <h2 style={{ color: 'white', textShadow: '0 0 3px black' }}>How would you rate your experience?</h2>
+                    <Rating
+                        value={rating} // Current rating value
+                        maxValue={5}  // Maximum number of stars
+                        onChange={(value) => setRating(value)} // Updates state when a star is clicked
+                    />                   
+                    <div style={{ color: 'white', marginTop: '20px' }}>
+                        You rated: {rating} / 5                      
+                    </div>
+                </Col>
+            </Row>
+            <Row className="px-4 my-5">
+                <Col><Button onClick={handleSubmitFeedback} variant="primary">Submit Feedback</Button>
+                </Col>
+                </Row>
             <style>
                 {`
                     body {
@@ -18,7 +55,7 @@ function FeedbackPage(){
                 `}
             </style>
         </Container>
-    )
+    );
 }
 
 export default FeedbackPage;
