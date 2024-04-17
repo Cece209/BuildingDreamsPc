14a24e620aeb686297a5377592a29ad1f20b313e
Layton00 <laytonlofton27@gmail.com>
2024-04-16 20:13:58 -0500

    Started configure front end

--- pc-builds/src/components/Pages/configure/ConfigurePage.js
+++ pc-builds/src/components/Pages/configure/ConfigurePage.js
@@ -2,12 +2,15 @@ import React from 'react';
 import { Container } from "react-bootstrap";
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import '../../../App.css';
+import { SearchField } from '@aws-amplify/ui-react';
+import { ScrollView } from '@aws-amplify/ui-react';
 
 function ConfigurePage(){
     return(
         <Container>
-             <Row className="px-4 my-5">
-             <Col><h1 style={{ color: 'white', textShadow: '0 0 3px black'}}>Configure Builds</h1></Col>
+             <Row className="px-1 my-3">
+             <Col className="text-center"><h3 style={{color: 'white', textShadow: '0 0 3px black'}}>Configure Builds</h3></Col>
                 </Row>
             <style>
                 {`
@@ -17,6 +20,87 @@ function ConfigurePage(){
                     }
                 `}
             </style>
+            <Row>
+            <Container>
+            <div className="ConfContainer">
+                <ScrollView height="500px" borderRadius="10px">
+                <div className="ConfLeft-column">
+                    <div>
+                    <Col><h6 style={{color: 'black', textShadow: '0 0 3px black'}}>GPU</h6></Col>
+                    <SearchField
+                        label="SearchGPU"
+                        placeholder="Search GPUs here..."
+                        />
+                    <p className="GPUArea">Text go here</p>
+                    </div>
+                    <div>
+                    <Col><h6 style={{color: 'black', textShadow: '0 0 3px black'}}>RAM</h6></Col>
+                    <SearchField
+                        label="SearchRAM"
+                        placeholder="Search RAM here..."
+                        />
+                    <p className="RAMArea">Text go here</p>
+                    </div>
+                    <div>
+                    <Col><h6 style={{color: 'black', textShadow: '0 0 3px black'}}>Case</h6></Col>
+                    <SearchField
+                        label="SearchCase"
+                        placeholder="Search Cases here..."
+                        />
+                    <p className="CaseArea">Text go here</p>
+                    </div>
+                    <div>
+                    <Col><h6 style={{color: 'black', textShadow: '0 0 3px black'}}>PSU</h6></Col>
+                    <SearchField
+                        label="SearchPSU"
+                        placeholder="Search PSUs here..."
+                        />
+                    <p className="CasePSU">Text go here</p>
+                    </div>
+                </div>
+                </ScrollView>
+                <div className="ConfMiddle-column">
+                    <img src="/img/PCSilh-removebg-preview.png"/>
+                </div>
+                <ScrollView height="500px" borderRadius="10px">
+                <div className="ConfRight-column">
+                    <div>
+                    <Col><h6 style={{color: 'black', textShadow: '0 0 3px black'}}>CPU</h6></Col>
+                    <SearchField
+                        label="SearchCPU"
+                        placeholder="Search CPUs here..."
+                        />
+                    <p className="CPUArea">Text go here</p>
+                    </div>
+                    <div>
+                    <Col><h6 style={{color: 'black', textShadow: '0 0 3px black'}}>Motherboard</h6></Col>
+                    <SearchField
+                        label="SearchMOBO"
+                        placeholder="Search MOBOS here..."
+                        />
+                    <p className="MOBOArea">Text go here</p>
+                    </div>
+                    <div>
+                    <Col><h6 style={{color: 'black', textShadow: '0 0 3px black'}}>Cooling System</h6></Col>
+                    <SearchField
+                        label="SearchCooling"
+                        placeholder="Search Cooling here..."
+                        />
+                    <p className="CoolArea">Text go here</p>
+                    </div>
+                    <div>
+                    <Col><h6 style={{color: 'black', textShadow: '0 0 3px black'}}>Main Memory</h6></Col>
+                    <SearchField
+                        label="SearchMEM"
+                        placeholder="Search Hard Drives/SSDs here..."
+                        />
+                    <p className="MemArea">Text go here</p>
+                    </div>
+                </div>
+                </ScrollView>
+            </div>
+            </Container>
+            </Row>
         </Container>
     )
 }
